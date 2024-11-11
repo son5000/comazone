c2fffdd2fe43bb4e7f3e725bd69dd2a97d8f1257
Hanje Son <sonhanje@kakao.com>
2024-11-11 21:57:07 +0900

    초기화 및 seed 데이터 구조화, CRUD API 구성

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ const app = express();
 app.use(express.json());
 
 app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany();
+  
+  const { offset = 0 , limit = 10 , order = 'newest'} = req.query;
+  let orderBy;
+  switch(order) {
+    case 'oldest':
+      orderBy = { createdAt : 'asc'};
+      break;
+    case 'newest':
+      default : orderBy = {createdAt : 'desc'};
+  }
+  const users = await prisma.user.findMany({
+    orderBy,
+    // offset => skip
+    skip : parseInt(offset),
+    // limit => take
+    take: parseInt(limit),
+  });
   res.send(users);
 });
 
@@ -23,18 +39,28 @@ app.get('/users/:id', async (req, res) => {
 
 app.post('/users', async (req, res) => {
   // 리퀘스트 바디 내용으로 유저 생성
+  const user = await prisma.user.create({
+    data : req.body,
+  });
   res.status(201).send(user);
 });
 
 app.patch('/users/:id', async (req, res) => {
   const { id } = req.params;
   // 리퀘스트 바디 내용으로 id에 해당하는 유저 수정
+  const user = await prisma.user.update({
+    where : { id },
+    data : req.body,
+  })
   res.send(user);
 });
 
 app.delete('/users/:id', async (req, res) => {
   const { id } = req.params;
   // id에 해당하는 유저 삭제
+  await prisma.user.delete({
+    where :  { id },
+  })
   res.sendStatus(204);
 });
 
